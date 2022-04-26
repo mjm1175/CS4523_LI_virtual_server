2d8fde3a8f7ad8e23299a9826d48fd5c53eac664
maria <mjm1175@nyu.edu>
2022-04-26 03:08:27 +0000

    fixed resume form

--- users/forms.py
+++ users/forms.py
@@ -36,27 +36,6 @@ class RegisterForm(UserCreationForm):
     password1 = forms.CharField(
                     help_text='Enter Password',
                     required=True,
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    uniqueId = models.CharField(max_length=100, null=True, blank=True)
-    # maybe remove upload_to??
-    image = models.ImageField(default='default.png', upload_to='profile_images')
-    email_confirmed = models.BooleanField(default=False)
-    # rm?
-    date_birth = models.DateField(blank=True, null=True)
-    # rm?
-    sex = models.CharField(max_length=100, choices=SEX_CHOICES, default=OTHER)
-    marital_status = models.CharField(max_length=100, choices=MARITAL_CHOICES, default=SINGLE)
-    address_line1 = models.CharField(max_length=100, null=True, blank=True)
-    address_line2 = models.CharField(max_length=100, null=True, blank=True)
-    city = models.CharField(max_length=100, null=True, blank=True)
-    state = models.CharField(max_length=100, choices=STATE_CHOICES, default=NY)
-    phone_number = models.CharField(max_length=12, null=True, blank=True)
-    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
-    date_created = models.DateTimeField(default=timezone.now)
-    last_updated = models.DateTimeField(null=True, blank=True)
-    cover_letter = models.FileField(upload_to='resumes', null=True, blank=True)
-    cv = models.FileField(upload_to='resumes', null=True, blank=True)
-                    widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}),
                     )
 
     password2 = forms.CharField(
